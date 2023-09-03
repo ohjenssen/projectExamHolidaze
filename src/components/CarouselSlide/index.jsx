bf700917c056029bb2609a544f8bbea20c702f8c
ohjenssen <oskar_jenssen@hotmail.com>
2023-09-03 11:45:28 +0200

    Added carousel slider

---
+++ src/components/CarouselSlide/index.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import PlaceHolderImage from '../../assets/No-Image-Placeholder.svg.png';
+
+function CarouselSlide({media}) {
+    const [index, setIndex] = useState(0);
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    };
+
+    return (
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+            {media && media.map((imgSrc) => (
+                <Carousel.Item>
+                    <div className='carousel-image-container'>
+                        <img src={imgSrc} key={imgSrc} className='carousel-image'/>
+                    </div>
+                </Carousel.Item>
+            ))}
+        </Carousel>
+    );
+}
+
+export default CarouselSlide;
