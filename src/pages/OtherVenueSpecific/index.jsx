358a01d1f28d99ea1ad5f6698811d1553ba1cc24
ohjenssen <oskar_jenssen@hotmail.com>
2023-09-03 13:01:37 +0200

    Changes to layout

--- src/pages/OtherVenueSpecific/index.jsx
+++ src/pages/OtherVenueSpecific/index.jsx
@@ -5,10 +5,11 @@ import { useParams } from "react-router-dom";
 import { Container, Row, Col, Spinner } from "react-bootstrap";
 import CarouselSlide from '../../components/CarouselSlide'
 import GuestCounter from '../../components/Counter';
+import Button from 'react-bootstrap/Button';
 
 export default function OtherVenueSpecific(){
     const { id } = useParams();
-    const url = `${constants.BASE_URL +  constants.VENUES}/${id}`;
+    const url = `${constants.BASE_URL +  constants.VENUES}/${id}?_bookings=true`;
 
     const { data, isLoading, isError } = useApi(url);
     console.log(data);
@@ -16,31 +17,54 @@ export default function OtherVenueSpecific(){
 
     return (
         <Container>
-            <Row>
+            
                 {isLoading && <Spinner />}
                 {isError && <div>Uh oh! Error message!</div>}
                 {data && 
-                <Col className="single-card">
-                    <h1>{data.name}</h1>
-                    {data.location && 
-                        <h4>{data.location.city}, {data.location.country}</h4>
-                    }
-                    {hasImages ? 
-                        (<CarouselSlide media={data.media}/>) : 
-                        (<div className="venue-img-container">
-                            <img src={PlaceHolderImage} className='venue-img'/>
-                        </div>
-                    )}
-                    <div>
-                        <h4>Max guests: {data.maxGuests}</h4>
-                        <p>How many guests: </p>
-                        <GuestCounter />
-                    </div>
-                </Col>
+                <>
+                    <Row>
+                        <Col className="single-card">
+                            <div>
+                                <h1>{data.name}</h1>
+                                {data.location && 
+                                    <h4>{data.location.city}, {data.location.country}</h4>
+                                }
+                                {hasImages ? 
+                                    (<CarouselSlide media={data.media}/>) : 
+                                    (<div className="venue-img-container">
+                                        <img src={PlaceHolderImage} className='venue-img'/>
+                                    </div>
+                                )}
+                                <div className='venue-information-container-1'>
+                                    <div>                 
+                                        <h4>Max guests: {data.maxGuests}</h4>
+                                        <div className='guests-container'>
+                                            <p>How many guests: </p>
+                                            <GuestCounter />
+                                        </div>
+                                        <p>Price:</p>
+                                        <h3>{data.price}kr</h3>
+                                    </div>
+                                    <div className='venue-information-booking'>
+                                        <div className="card-rating">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-star-fill" viewBox="0 0 16 16">
+                                                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                            </svg>
+                                            <p>{data.rating}</p>
+                                        </div>
+                                        <Button>Book</Button>
+                                    </div>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col className="single-card">
+                            <h4>Dates</h4>
+                            <a>See bookings</a>
+                        </Col>
+                    </Row>
+                </>
             }
-            </Row>
-            <Row>
-            </Row>
+
         </Container>
     )
 }
