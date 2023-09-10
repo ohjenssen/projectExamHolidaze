bca6895b48343a9418b4fbb9113d36338eb885e1
ohjenssen <oskar_jenssen@hotmail.com>
2023-09-10 18:08:27 +0200

    toggle venuemanager function

---
+++ src/components/Forms/BecomeVenueManager/index.jsx
@@ -0,0 +1,100 @@
+import { constants } from "../../../js/constants";
+import { Button, Card, Form, Modal } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+export default function BecomeVenuemanager(){
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const isManager = JSON.parse(localStorage.getItem('venueManager'));
+    const { profilename } = useParams();
+    const url = `${constants.BASE_URL}${constants.PROFILES}/${profilename}`;
+
+
+    const options = {
+        method: "PUT",
+        body: JSON.stringify({ 
+            venueManager: isManager ? false : true,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            "Authorization": `Bearer ${JSON.parse(localStorage.getItem('accessToken'))}`,
+        }
+    }
+
+    async function becomeVenueManager(event){
+        event.preventDefault();
+        try {
+            const response = await fetch(url, options);
+            const json = await response.json();
+            console.log(response);
+            if(response.ok){
+                localStorage.setItem('venueManager', JSON.stringify(json.venueManager));
+                window.location.reload();
+            }
+        } catch (error){
+            console.log(error)
+        }
+    }
+
+    return (
+        <>
+            {isManager ? 
+                    <div className="profile-info">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-check-fill" viewBox="0 0 16 16">
+                            <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z"/>
+                            <path d="m8 3.293 4.712 4.712A4.5 4.5 0 0 0 8.758 15H3.5A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z"/>
+                            <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.707l.547.547 1.17-1.951a.5.5 0 1 1 .858.514Z"/>
+                        </svg> 
+                        <p>Venuemanager</p>
+                        <Button variant="primary" className="profile-edit-btn" onClick={handleShow}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                            </svg>
+                        </Button>
+
+                        <Modal show={show} onHide={handleClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Stop being a venuemanager?</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>You can always beceome one again.</Modal.Body>
+                            <Modal.Footer>
+                                <Form onSubmit={becomeVenueManager}>
+                                    <Button variant="primary" type="submit">
+                                        Yes
+                                    </Button>
+                                    <Button variant="danger" onClick={handleClose}>
+                                        No
+                                    </Button>
+                                </Form>
+                            </Modal.Footer>
+                        </Modal>
+                    </div> 
+                    :
+                    <>
+                        <Button variant="primary" onClick={handleShow}>
+                            Become venuemanager
+                        </Button>
+
+                        <Modal show={show} onHide={handleClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Become a venuemanager?</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>Prepare for a dazzling jounrey!</Modal.Body>
+                            <Modal.Footer>
+                                <Form onSubmit={becomeVenueManager}>
+                                    <Button variant="primary" type="submit">
+                                        Yes
+                                    </Button>
+                                    <Button variant="danger" onClick={handleClose}>
+                                        No
+                                    </Button>
+                                </Form>
+                            </Modal.Footer>
+                        </Modal>
+                    </>
+                }
+        </>
+    )
+}
--- src/components/Profilecard/index.jsx
+++ src/components/Profilecard/index.jsx
@@ -3,77 +3,29 @@ import { constants } from '../../js/constants';
 import useGetProfile from "../../hooks/useGetProfile";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import BecomeVenuemanager from "../Forms/BecomeVenueManager";
 
 export default function Profilecard(){
     const { profilename } = useParams();
     const url = `${constants.BASE_URL}${constants.PROFILES}/${profilename}`;
     const { profile } = useGetProfile(url);
-    const isManager = JSON.parse(localStorage.getItem('venueManager'));
-
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    const options = {
-        method: "PUT",
-        body: JSON.stringify({ 
-            venueManager: true,
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-            "Authorization": `Bearer ${JSON.parse(localStorage.getItem('accessToken'))}`,
-        }
-    }
-
-    async function submitHandler(event){
-        event.preventDefault();
-        try {
-            const response = await fetch(url, options);
-            const json = await response.json();
-            console.log(response);
-            if(response.ok){
-                localStorage.setItem('venueManager', JSON.stringify(json.venueManager));
-                window.location.reload();
-            }
-        } catch (error){
-            console.log(error)
-        }
-    }
 
     return (
         <Card style={{ width: '18rem' }}>
             <Card.Body>
                 <Card.Title>{profile.name}</Card.Title>
                 <Card.Text>
-                    Mail: {profile.email}
+                    <div className="profile-info">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-envelope-at-fill" viewBox="0 0 16 16">
+                            <path d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2H2Zm-2 9.8V4.698l5.803 3.546L0 11.801Zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586l-1.239-.757ZM16 9.671V4.697l-5.803 3.546.338.208A4.482 4.482 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671Z"/>
+                            <path d="M15.834 12.244c0 1.168-.577 2.025-1.587 2.025-.503 0-1.002-.228-1.12-.648h-.043c-.118.416-.543.643-1.015.643-.77 0-1.259-.542-1.259-1.434v-.529c0-.844.481-1.4 1.26-1.4.585 0 .87.333.953.63h.03v-.568h.905v2.19c0 .272.18.42.411.42.315 0 .639-.415.639-1.39v-.118c0-1.277-.95-2.326-2.484-2.326h-.04c-1.582 0-2.64 1.067-2.64 2.724v.157c0 1.867 1.237 2.654 2.57 2.654h.045c.507 0 .935-.07 1.18-.18v.731c-.219.1-.643.175-1.237.175h-.044C10.438 16 9 14.82 9 12.646v-.214C9 10.36 10.421 9 12.485 9h.035c2.12 0 3.314 1.43 3.314 3.034v.21Zm-4.04.21v.227c0 .586.227.8.581.8.31 0 .564-.17.564-.743v-.367c0-.516-.275-.708-.572-.708-.346 0-.573.245-.573.791Z"/>
+                        </svg> 
+                        {profile.email}
+                    </div>
                 </Card.Text>
-
-                {isManager ? 
-                <p>You is manager</p> :
-                    <>
-                    <Button variant="primary" onClick={handleShow}>
-                        Become venuemanager
-                    </Button>
-
-                    <Modal show={show} onHide={handleClose}>
-                            <Modal.Header closeButton>
-                                <Modal.Title>Become a venuemanager?</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>Prepare for a dazzling jounrey!</Modal.Body>
-                            <Modal.Footer>
-                                <Form onSubmit={submitHandler}>
-                                    <Button variant="primary" type="submit">
-                                        Yes
-                                    </Button>
-                                    <Button variant="danger" onClick={handleClose}>
-                                        No
-                                    </Button>
-                                </Form>
-                            </Modal.Footer>
-                        </Modal>
-                    </>
-                }
+                <BecomeVenuemanager />
                 <Card.Img variant="top" src={profile.avatar} />
+                <p>Edit avatar</p>
             </Card.Body>
         </Card>
     )
