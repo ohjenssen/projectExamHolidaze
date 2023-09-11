bd0a5603577fc7bce18d5c57f5236221f2f45cb9
ohjenssen <oskar_jenssen@hotmail.com>
2023-09-11 19:15:56 +0200

    User may view upcoming bookings

---
+++ src/pages/MyBookingsPage/index.jsx
@@ -0,0 +1,47 @@
+import { Container, Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { constants } from '../../js/constants';
+import useGetBookingsByProfile from '../../hooks/useGetBookingsByProfile';
+import BookingCards from '../../components/VenueCards/BookingCards';
+
+export default function MyBookingsPage(){
+    const options = {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            "Authorization": `Bearer ${JSON.parse(localStorage.getItem('accessToken'))}`,
+        }
+    }
+
+    const profileName = JSON.parse(localStorage.getItem('profileName'));
+    const url = `${constants.BASE_URL + constants.PROFILES}/${profileName}/bookings?_venue=true`;
+    const { data, isLoading, isError } = useGetBookingsByProfile(url, options);
+    const isManager = JSON.parse(localStorage.getItem('venueManager'));
+
+    return (
+        <>
+            <Row>
+                <Col className='profile-page'>
+                    <div className='profile-nav-links'>
+                        <Link to={`/profilepage/${JSON.parse(localStorage.getItem("profileName"))}`}>Profile</Link>
+                        <Link to={`/mybookings/${JSON.parse(localStorage.getItem('profileName'))}`}>Bookings</Link>
+                       {isManager && <Link>Venues</Link>}
+                    </div>
+                    <h1>These are your bookings</h1>
+                </Col>
+            </Row>
+            <Row className='d-flex'>
+                {data.map((booking) => (
+                        <BookingCards 
+                            key={booking.id}
+                            id={booking.id}
+                            from={booking.dateFrom} 
+                            to={booking.dateTo}
+                            guests={booking.guests}
+                            venue={booking.venue}
+                        />
+                    ))}
+            </Row>
+        </>
+    )
+}
