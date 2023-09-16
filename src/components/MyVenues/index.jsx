af42511529792a94e6cd505e1376550bf96f21d5
ohjenssen <oskar_jenssen@hotmail.com>
2023-09-16 14:28:23 +0200

    User can view their own venues

--- src/components/MyVenues/index.jsx
+++ src/components/MyVenues/index.jsx
@@ -1,6 +1,16 @@
+import UserVenueCards from '../VenueCards/UserVenueCards';
+import useGetVenues from '../../hooks/useGetVenue';
+
 export default function MyVenues(){
+
+    const { data, isLoading, isError } = useGetVenues();
+
     return (
         <>
+            {data && 
+                data.map((venue) => (
+                    <UserVenueCards venue={venue}/>
+            ))}
         </>
     )
 }
